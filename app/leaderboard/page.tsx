92be12cbf1655009bb33ff818a823ae87abfd93f
ILostMyMicrowave <danielboneanstey@gmail.com>
2026-08-04 15:41:12 +0100

    Update page.tsx

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -2,7 +2,6 @@
 
 import Navbar from "@/components/Navbar";
 import WarHistoryDropdown from "@/components/WarHistoryDropdown";
-import AssistantBubble from "@/components/AssistantBubble";
 import { useEffect, useMemo, useRef, useState, type CSSProperties, type ReactNode } from "react";
 
 export const dynamic = "force-dynamic";
@@ -2147,7 +2146,6 @@ export default function LeaderboardPage() {
           }
         `}</style>
       </main>
-      <AssistantBubble />
     </>
   );
 }
