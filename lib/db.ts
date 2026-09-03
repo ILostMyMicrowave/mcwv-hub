4a21ceb6af5c3f8d67d8c84e75b618f6afcec1b0
ILostMyMicrowave <danielboneanstey@gmail.com>
2026-09-03 22:03:03 +0100

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -51,7 +51,7 @@ function getPool() {
     keepAlive: true,
     keepAliveInitialDelayMillis: 10_000,
     allowExitOnIdle: true,
-    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: true, ca: undefined } : undefined,
+    ssl: process.env.NODE_ENV === "production" ? { rejectAuthorized: false, ca: undefined } : undefined,
   }
 
   const pool = new Pool(config)
